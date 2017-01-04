0f69140d0bb75f6c624bf3c090223a2d7d233ffd
Drgravy <drg113001@gmail.com>
2017-01-04 13:10:48 -0600

    revert: d850/d851/d852/d855/ls990/vs985: Regenerate makefiles
    (reverted from commit db28f821840f7c836071b5a30961bb4c9a96fdc1)
    
    Change-Id: I86b96d1fdfd1d821fea8ea5d85bd879048c745bb

--- vs985/vs985-vendor.mk
+++ vs985/vs985-vendor.mk
@@ -33,6 +33,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/vs985/proprietary/etc/firmware/venus.b04:system/etc/firmware/venus.b04 \
     vendor/lge/vs985/proprietary/etc/firmware/venus.mbn:system/etc/firmware/venus.mbn \
     vendor/lge/vs985/proprietary/etc/firmware/venus.mdt:system/etc/firmware/venus.mdt \
+    vendor/lge/vs985/proprietary/lib/libmdmdetect.so:system/lib/libmdmdetect.so \
     vendor/lge/vs985/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/lge/vs985/proprietary/lib/libuicc.so:system/lib/libuicc.so \
     vendor/lge/vs985/proprietary/lib/libvss_common_idl.so:system/lib/libvss_common_idl.so \
@@ -50,6 +51,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/vs985/proprietary/vendor/lib/libdsutils.so:system/vendor/lib/libdsutils.so \
     vendor/lge/vs985/proprietary/vendor/lib/libidl.so:system/vendor/lib/libidl.so \
     vendor/lge/vs985/proprietary/vendor/lib/libnetmgr.so:system/vendor/lib/libnetmgr.so \
+    vendor/lge/vs985/proprietary/vendor/lib/libperipheral_client.so:system/vendor/lib/libperipheral_client.so \
     vendor/lge/vs985/proprietary/vendor/lib/libqcci_legacy.so:system/vendor/lib/libqcci_legacy.so \
     vendor/lge/vs985/proprietary/vendor/lib/libqdi.so:system/vendor/lib/libqdi.so \
     vendor/lge/vs985/proprietary/vendor/lib/libqdp.so:system/vendor/lib/libqdp.so \
@@ -63,7 +65,3 @@ PRODUCT_COPY_FILES += \
     vendor/lge/vs985/proprietary/vendor/lib/libqmiservices.so:system/vendor/lib/libqmiservices.so \
     vendor/lge/vs985/proprietary/vendor/lib/libril-qc-qmi-1.so:system/vendor/lib/libril-qc-qmi-1.so \
     vendor/lge/vs985/proprietary/vendor/lib/libril-qcril-hook-oem.so:system/vendor/lib/libril-qcril-hook-oem.so
-
-PRODUCT_PACKAGES += \
-    libmdmdetect \
-    libperipheral_client
